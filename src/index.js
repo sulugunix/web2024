82c47295e964a36a5cc2ebac771db4f258bf2b34
suluguni <suluguni74@gmail.com>
2024-12-21 03:46:43 +0500

    lesson7

--- src/index.js
+++ src/index.js
@@ -9,3 +9,22 @@ menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("menu-icon--active");
     header.classList.toggle("header--mobile");
 })
+
+const faqIcons = document.querySelectorAll(".faq-icon");
+const texts = document.querySelectorAll(".faq-text");
+
+for (let i = 0; i < faqIcons.length; i++) {
+    faqIcons[i].addEventListener("click", () => {
+        faqIcons[i].classList.toggle("faq-icon--active");
+        for (let j = 0; j < texts.length; j++) {
+            if (i == j) {
+                if (texts[j].classList.contains("hidden")) {
+                    texts[j].classList.remove("hidden");
+                }
+                else {
+                    texts[j].classList.add("hidden");
+                }
+            }
+        }
+    })
+}
